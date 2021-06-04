c1d60ae5554c2205529e7510b67c64915c794800
Grigoriy <volchonokgrisha@gmail.com>
2021-06-04 11:10:49 +0300

    Initial commit

---
+++ LogParse/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿using System.Windows.Forms;
+
+namespace LogParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxParseResult = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.labelSignatureName = new System.Windows.Forms.Label();
+            this.comboBoxSignature = new System.Windows.Forms.ComboBox();
+            this.buttonGetLog = new System.Windows.Forms.Button();
+            this.buttonHighlightSignature = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxParseResult
+            // 
+            this.richTextBoxParseResult.Location = new System.Drawing.Point(12, 224);
+            this.richTextBoxParseResult.Name = "richTextBoxParseResult";
+            this.richTextBoxParseResult.Size = new System.Drawing.Size(758, 150);
+            this.richTextBoxParseResult.TabIndex = 0;
+            this.richTextBoxParseResult.Text = "";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(12, 391);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(758, 150);
+            this.richTextBoxLog.TabIndex = 1;
+            this.richTextBoxLog.Text = "";
+            // 
+            // labelSignatureName
+            // 
+            this.labelSignatureName.Location = new System.Drawing.Point(12, 9);
+            this.labelSignatureName.Name = "labelSignatureName";
+            this.labelSignatureName.Size = new System.Drawing.Size(100, 30);
+            this.labelSignatureName.TabIndex = 2;
+            this.labelSignatureName.Text = "Сигнатура:";
+            // 
+            // comboBoxSignature
+            // 
+            this.comboBoxSignature.FormattingEnabled = true;
+            this.comboBoxSignature.Location = new System.Drawing.Point(12, 42);
+            this.comboBoxSignature.Name = "comboBoxSignature";
+            this.comboBoxSignature.Size = new System.Drawing.Size(250, 24);
+            this.comboBoxSignature.TabIndex = 3;
+            this.comboBoxSignature.KeyDown += new System.Windows.Forms.KeyEventHandler(this.comboBoxSignature_KeyDown);
+            // 
+            // buttonGetLog
+            // 
+            this.buttonGetLog.Location = new System.Drawing.Point(12, 173);
+            this.buttonGetLog.Name = "buttonGetLog";
+            this.buttonGetLog.Size = new System.Drawing.Size(120, 45);
+            this.buttonGetLog.TabIndex = 4;
+            this.buttonGetLog.Text = "открыть лог-журнал";
+            this.buttonGetLog.UseVisualStyleBackColor = true;
+            this.buttonGetLog.Click += new System.EventHandler(this.buttonGetLog_Click);
+            // 
+            // buttonHighlightSignature
+            // 
+            this.buttonHighlightSignature.Location = new System.Drawing.Point(142, 173);
+            this.buttonHighlightSignature.Name = "buttonHighlightSignature";
+            this.buttonHighlightSignature.Size = new System.Drawing.Size(120, 45);
+            this.buttonHighlightSignature.TabIndex = 5;
+            this.buttonHighlightSignature.Text = "выделить сигнатуру";
+            this.buttonHighlightSignature.UseVisualStyleBackColor = true;
+            this.buttonHighlightSignature.Click += new System.EventHandler(this.buttonHighlightSignature_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 565);
+            this.Controls.Add(this.buttonHighlightSignature);
+            this.Controls.Add(this.buttonGetLog);
+            this.Controls.Add(this.comboBoxSignature);
+            this.Controls.Add(this.labelSignatureName);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.richTextBoxParseResult);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogParse";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.ComboBox comboBoxSignature;
+        private System.Windows.Forms.Button buttonGetLog;
+        private System.Windows.Forms.Button buttonHighlightSignature;
+
+        private System.Windows.Forms.Label labelSignatureName;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.RichTextBox richTextBoxParseResult;
+
+        #endregion
+    }
+}
---
+++ LogParse/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace LogParse
+{
+    public partial class Form1 : Form
+    {
+        public MatchCollection myMatch;
+        Dictionary<string, Color> signatures = new Dictionary<string, Color>();
+        public string signatura;
+        public string fileText;
+        public Form1()
+        {
+            InitializeComponent();
+            this.signatures.Add("form", Color.Red);
+            this.comboBoxSignature.Items.Add("form");
+            this.signatures.Add("string", Color.Blue);
+            this.comboBoxSignature.Items.Add("string");
+        }
+
+        private void buttonHighlightSignature_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBoxSignature.Items.Count == 0)
+                {
+                    throw new Exception("Сигнатура не может быть пустой!");
+                }
+
+                if (comboBoxSignature.SelectedItem == null)
+                {
+                    throw new Exception("Необходимо выбрать сигнатуру!");
+                }
+                
+                if (String.IsNullOrEmpty(richTextBoxLog.Text))
+                {
+                    throw new Exception("Необходимо выбрать хотя бы один лог-журнал!");
+                }
+
+                var buf = comboBoxSignature.SelectedItem.ToString();
+
+                if (!signatures.ContainsKey(buf))
+                {
+                    Random rand = new Random();
+                    Color tmp = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+                    while (signatures.ContainsValue(tmp))
+                    {
+                        tmp = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+                    }
+                    signatures.Add(buf, tmp);
+                }
+                
+                signatura = buf;
+                
+                Regex regex = new Regex(@signatura);
+                MatchCollection matches = regex.Matches(fileText);
+                
+                myMatch = matches;
+                
+                richTextBoxParseResult.Text = "Все вхождения строки "
+                                   + signatura + " в исходном тексте: " + "\r\n";
+                
+                if (matches.Count > 0)
+                {
+                    foreach (Match match in matches)
+                    {
+                        richTextBoxParseResult.Text += match.Index + " " + match.Value + "\r\n";
+                        Color value;
+                        if (!signatures.TryGetValue(buf, out value))
+                            throw new Exception("Невозможно получить значение цвета!");
+                        SetSelectionStyle(match.Index, match.Length, FontStyle.Underline, value);
+                    }
+                }
+                else
+                {
+                    richTextBoxParseResult.Text += "Совпадений не найдено" + "\r\n";
+                }
+                
+                
+                MessageBox.Show("Анта бака!\nВсе хорошо\n", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception excptn)
+            {
+                MessageBox.Show("Анта бака!\nОшибка при работе с сигнатурой!\n" + excptn.Message + excptn.StackTrace, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void comboBoxSignature_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (comboBoxSignature.Items.Count > 0 && !String.IsNullOrEmpty(comboBoxSignature.Text))
+                {
+                    bool isExist = false;
+                    foreach (var item in comboBoxSignature.Items)
+                    {
+                        if (item.ToString() == comboBoxSignature.Text)
+                            isExist = true;
+                    }
+                    if (!isExist)
+                        comboBoxSignature.Items.Add(comboBoxSignature.Text);
+                }
+                else
+                {
+                    if (!String.IsNullOrEmpty(comboBoxSignature.Text))
+                        comboBoxSignature.Items.Add(comboBoxSignature.Text);
+                }
+            }
+        }
+
+        private void buttonGetLog_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*";
+                openFileDialog.Multiselect = true;
+                
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        foreach (var filePath in openFileDialog.FileNames)
+                        {
+                            richTextBoxLog.Text += File.ReadAllText(filePath, Encoding.Default);
+                        }
+
+                        fileText = richTextBoxLog.Text;
+                    }
+                    catch (Exception excptn)
+                    {
+                        MessageBox.Show("Анта бака!\nОшибка при считывании лог-файла!\n" + excptn.Message + excptn.StackTrace, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        
+        private void SetSelectionStyle(int startIndex, int length, FontStyle style, Color color)
+        {
+            richTextBoxLog.Select(startIndex, length);
+            richTextBoxLog.SelectionFont = new Font(richTextBoxLog.SelectionFont,
+                richTextBoxLog.SelectionFont.Style | style);
+            richTextBoxLog.SelectionColor = color;
+        }
+    }
+}
